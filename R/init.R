8040491e66f4078ee5d8f2766194fab9a824bc44
Eliezer Gurarie <egurarie@umd.edu>
2017-04-12 13:26:18 +0000

    version 0.0-2

---
+++ R/init.R
@@ -0,0 +1,13 @@
+#' @importFrom zoo zoo
+#' @import graphics
+#' @importFrom Matrix solve
+#' @importFrom stats AIC arima dnorm kmeans lm optim pchisq qqnorm quantile rnorm sd smooth var
+#' @importFrom grDevices grey rgb
+#' @import plyr
+#' @import mvtnorm
+#' @import RColorBrewer
+#' @import minpack.lm
+#' @import numDeriv
+#' @import magrittr
+#' @importFrom scales alpha
+NULL
